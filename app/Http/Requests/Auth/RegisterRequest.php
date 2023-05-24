c962f43f46a1a4c9a070637ec6ddde8bc1571c58
coraline-it <c.chau@it-students.fr>
2023-05-24 09:20:56 +0200

    modif auth email

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -27,7 +27,7 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255'],
             'password' => ['required', 'string'],
             'name' => ['required', 'string'],
             'first_name' => ['required', 'string'],
