09f8acef9a5f41c9f0b1a5a652407cb3723019b3
Alex <alexhusien@gmail.com>
2023-05-24 10:20:54 +0200

    Alex

--- app/Http/Middleware/AdminAuthenticated.php
+++ app/Http/Middleware/AdminAuthenticated.php
@@ -5,8 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-
-class AdminAuthenticated​
+use Illuminate\Support\Facades\Auth;
+class AdminAuthenticated
 {
   /**
      * Handle an incoming request.
